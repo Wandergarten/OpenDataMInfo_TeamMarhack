2e6f46863ccb1ceb30470daca64d5a527722b53a
KaroFr <frohnapfelkarolin@gmail.com>
2021-04-18 01:56:43 +0200

    .

--- Code/stats_Karo.R
+++ Code/stats_Karo.R
@@ -23,6 +23,7 @@ for(i in 1:6){
   player_stat <- rbind(player_stat, new_entry)
 }
 
+#plot
 win_loose_melt <- melt(data = player_stat[,c(1:3)], id.vars = 'player')
 data_sorted <- arrange(win_loose_melt, desc(variable), player) 
 data_cumsum <- ddply(data_sorted, "player", transform, label_ypos=cumsum(value))
@@ -33,25 +34,25 @@ ggplot(data_cumsum, aes(x = player, y = value, fill = variable)) +
   guides(fill=guide_legend(title=NULL)) +
   labs(title = "Plot of won and lost games", x = "Player", y = "Number of Games") + 
   theme_classic()
-
 ###############################################################################
 # mean number of game duration
 ###############################################################################
 mean_duration <- data.table()
-for (i in 1:6) {
+for (i in 1:length(players)) {
   time_stamps <- dt[(player1==21 | player2==21) & (player1_id==players[i] | player2_id==players[i])]
-  mean_tmp <- (sum(time_stamps[,3])/nrow(time_stamps))/60
+  mean_tmp <- round((sum(time_stamps[,3])/nrow(time_stamps))/60, digits = 2)
   mean_duration <- rbind(mean_duration, mean_tmp)
 }
 player_stat <- cbind(player_stat, mean_duration = mean_duration)
+names(player_stat)[4] <- "mean_duration"
 
+#plot
 mean_duration_melt <- melt(data = player_stat[,c(1,4)], id.vars = 'player')
 ggplot(mean_duration_melt, aes(x = player, y = value)) + 
   geom_bar(stat = 'identity', width=0.6, fill="steelblue") + 
   labs(title = "Plot of the mean duration of one game in minutes", x = "Player", y = "Mean Duration") + 
-  geom_text(aes(label = round(value, digits = 2)), vjust=1.6, color="white") +
+  geom_text(aes(label = value), vjust=1.6, color="white") +
   theme_classic()
-
 ###############################################################################
 # number of rounds per game
 ###############################################################################
@@ -60,11 +61,12 @@ dt[,rounds_per_game := .N, by = list(game_id, game_date)]
 ###############################################################################
 # total number of games per day
 ###############################################################################
-dt[(player1==21 | player2==21), games_per_day := .N, by = C(game_date, player1)]
+dt[(player1==21 | player2==21), games_per_day := .N, by = C(player1_id)]
 
 ###############################################################################
 # number of games per couple
 ###############################################################################
+#compute number of games playes by two people
 result <- c()
 for (i in 1:length(players)) {
   for (j in 1:length(players)) {
@@ -76,6 +78,7 @@ love_matrix <- matrix(result, byrow = TRUE, nrow = length(players))
 colnames(love_matrix) <- players
 rownames(love_matrix) <- players
 
+#plot it
 love_melt <- melt(love_matrix)
 names(love_melt)[3] <- "Amount"
 ggplot(love_melt, aes(x = Var2, y = Var1)) +
@@ -89,5 +92,18 @@ ggplot(love_melt, aes(x = Var2, y = Var1)) +
         plot.title=element_text(size=16))
 
 ###############################################################################
-# number of games per Person
+# number of games per Session/Day
 ###############################################################################
+games_per_day <- data.table()
+for (i in 1:length(players)) {
+  date_data <- dt[(player1==21 | player2==21) & (player1_id==players[i] | player2_id==players[i]), 
+                  games_per_date:=.N, by = game_date][games_per_date!="NA"]
+  games_per_day <- rbind(games_per_day, mean(date_data[,games_per_date]))
+}
+player_stat <- cbind(player_stat, games_per_day = games_per_day)
+names(player_stat)[5] <- "games_per_day"
+
+
+
+
+
