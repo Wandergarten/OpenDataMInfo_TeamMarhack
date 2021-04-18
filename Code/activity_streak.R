21aad26f8b62f87b991d4d04fdb290bb6f67421c
= <=>
2021-04-18 03:36:12 +0200

    final push day1

--- Code/activity_streak.R
+++ Code/activity_streak.R
@@ -1,16 +1,16 @@
 library(data.table)
 library(zoo)
 
-get_streak <- function(dt,player,input_date,streak = 0){
-    input_date = as.Date(input_date)
-    start_date = as.Date(sort(unique(dt[,game_date]))[1])
-    games_played <- dt[as.Date(game_date) == input_date,]
-    players_played <- unique(unlist(list(unique(games_played[,player2_id]),unique(games_played[,player1_id])))) 
-    if((player %in% players_played) & (input_date > start_date)){
-        get_streak(dt,player,input_date - 1,streak + 1)
-    }else{
-        return(streak)
-    }
+get_streak <- function(dt, player, input_date, streak = 0) {
+  input_date = as.Date(input_date)
+  start_date = as.Date(sort(unique(dt[, game_date]))[1])
+  games_played <- dt[as.Date(game_date) == input_date,]
+  players_played <- unique(unlist(list(unique(games_played[, player2_id]), unique(games_played[, player1_id]))))
+  if ((player %in% players_played) & (input_date > start_date)) {
+    get_streak(dt, player, input_date - 1, streak + 1)
+  } else {
+    return(streak)
+  }
 }
 
 
@@ -20,4 +20,4 @@ get_streak <- function(dt,player,input_date,streak = 0){
 
 dt <- read.csv(file = "./Data/game_simulation_data.csv")
 dt <- data.table(dt)
-get_streak(dt,"Luis","2020-09-01")
+get_streak(dt, "Jonny", "2020-09-01")
